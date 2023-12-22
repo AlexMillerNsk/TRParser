dab15d4a697ddd8969b66c10b43352f605e5e8f4
amelnikov <amelnikov@PC100127>
2023-12-22 15:17:35 +0700

    names update

--- TRParser/Program.fs
+++ TRParser/Program.fs
@@ -1,91 +1,95 @@
 ﻿
 open FSharp.Interop.Excel
 open FSharp.Json
+open System.IO
 open Types
 
 
 type DataTypesTest = ExcelFile<"Grafik.xlsx",ForceString=true>
 type Row = DataTypesTest.Row
 
-
-
-
-//Make convenience methods
-
 let file = new DataTypesTest()
 
-
-
 let notIsNull (row:Row) = row.``№ п/п`` |> isNull |> not
-
-let validate x = 
-    let w = string x
-    match w with
-    | "0"       -> None
-    | ""        -> None
-    | "Рзаб"    -> None
-    | "тех огр" -> None
-    | _         -> printf $"{x}"
-                   Some x 
              
 let validateDebit (x:obj) = 
     let w = x|>string
     match w with
     | "0"    -> None
-    | "12"   -> printfn "12"
-                Some 12
-    | "24"   -> printfn "24"
-                Some 24
-    | _    -> None
+    | "12"   -> Some 12              
+    | "24"   -> Some 24               
+    | _      -> None
 
- 
-let parseProduction (x:int) (r:Row) = validateDebit (r.GetValue(x)) 
+let validatePressure (x:obj) = 
+    let w = x|>string
+    match w with
+    | "Рзаб" -> Some 12             
+    | _      -> None
 
-//let myFun() =
-//    for index in {13..23} do parseProduction index (r:Row)
-        
-let myFun x =
-    for index in {12..41} do 
-    let process = parseProduction index x  
-    let itog = process.Value
+let validateMilling (x:obj) = 
+    let w = x|>string
+    match w with
+    | "1"    -> Some 12             
+    | _      -> None 
 
+let validateMethanol (x:obj) = 
+    let w = x|>string
+    match w with
+    | "X"    -> Some 12             
+    | _      -> None 
+
+let validateFluidSampling (x:obj) = 
+    let w = x|>string
+    match w with
+    | "%"    -> Some 12
+    | "П"    -> Some 12
+    | _      -> None 
+
+let parseProduction (validateFunc: obj -> int option) (x:int) (r:Row) = validateFunc (r.GetValue(x)) 
+       
+let createProcess func (x:Row) processName =
+    for index in {13..43} do 
+    if (parseProduction func index x).IsSome then
+        let duration = (parseProduction func index x).Value
+        let date index = 
+            let day = index - 10
+            if day <10 then $"0{day}" else  string day
+        let day = date index
+        let wellInfo = WellInfoConstructor.Create x.скв x.куст processName duration $"2023-07-{day} 00:00:00.000 +0700"
+        let json = Json.serialize wellInfo
+        let path =
+            Path.Combine(Path.GetTempPath(), "Sampl546.txt")       
+        use sw = new StreamWriter(new FileStream(path, FileMode.Append, FileAccess.Write))
+        sw.WriteLineAsync(json)|>ignore
+    else ()
 
 
 let parseProcess (r:Row) = 
     match r.процесс with
     | "Подъем флюида"              -> let processName = "FluidLifting"
-                                      printf $"{processName}"                                     
-    | "Замер дебита"               -> let processName = "MeasuremebtDebit"
-                                      printf $"{processName}"
-                                      createProcess r
+                                      printf "0"                                     
+    | "Замер дебита"               -> let processName = "MeasurementDebit"                                      
+                                      createProcess validateDebit r processName
     | "замер забойного"            -> let processName = "BottomHolePressure"
-                                      printf $"{processName}"
+                                      createProcess validatePressure r processName
     | "Фрезерование Этанол"        -> let processName = "Milling"
-                                      printf $"{processName}"
+                                      createProcess validateMilling r processName
     | "фрезерование Кредит-Альянс" -> let processName = "Milling"
-                                      printf $"{processName}"
+                                      createProcess validateMilling r processName
     | "фрезерование ОТК"           -> let processName = "Milling"
-                                      printf $"{processName}"
+                                      createProcess validateMilling r processName
     | "метанольная обработка"      -> let processName = "Methanol"
-                                      printf $"{processName}"
+                                      createProcess validateMethanol r processName
     | "тепловая обработка"         -> let processName = "Treasurement"
-                                      printf $"{processName}"
-    | "Отбор проб"                 -> let processName = "zabil kak "
-                                      printf $"{processName}"
-    | _                            -> let processName = "something wrong"
-                                      printf $"{processName}"
-
-//let row = file.Data |> Seq.filter notIsNull |> List.ofSeq|>List.head|> List.map (fun x -> parseProduction x)
-
-let row = file.Data |> Seq.filter notIsNull |> List.ofSeq
-
+                                      printf "1"
+    | "Отбор проб"                 -> let processName = "FluidSampling"
+                                      createProcess validateFluidSampling r processName
+    | _                            -> printf "smthn wrong"
+                                      
 let row1 = file.Data |> Seq.filter notIsNull |> List.ofSeq|>List.map (fun x -> parseProcess x)
 
 
 
 
 
-//myFun r0
-//myFun r1
-//myFun r2
-//myFun r3
+
